b8706ed6e3837ece036aabbd25e3094d96633d16
我只想看看蓝天 <1207032539@qq.com>
2022-05-21 23:36:56 +0800

    初步开发调测

--- code/api/build_gallery.py
+++ code/api/build_gallery.py
@@ -1,5 +1,17 @@
 import deploy.python.build_gallery
 import setting
+from tool import common
+
+db = common.get_db()
+
+res = db.table('image as a') \
+    .join('label as b', 'a.label_id', '=', 'b.id') \
+    .select('b.name', 'a.path') \
+    .lists('name', 'path')
+
+with open('./static/gallery/label.txt', 'w', encoding='utf-8') as f:
+    for path in res:
+        f.write(f"{path} {res[path]}\n")
 
 config = setting.PP_CONFIG
 deploy.python.build_gallery.main(config)
